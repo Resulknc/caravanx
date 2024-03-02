08980af91749bc7cedfdd27813b7ac084f85d8a7
Resul <resulekinci10@gmail.com>
2024-03-02 13:34:29 +0300

    First commit

---
+++ src/main/java/com/project/caryatch/CaryatchApplication.java
@@ -0,0 +1,13 @@
+package com.project.caryatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaryatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaryatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/caryatch/CaryatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.caryatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaryatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
